4e5f7a01ecaa88e4f3c2550c5be4ba5ba2c496a1
Melissa9mpenzi <melissampenzi@gmail.com>
2024-10-21 15:15:03 +0300

    First commit

---
+++ factorial.py
@@ -0,0 +1,19 @@
+# def factorial(n):
+#     if n == 0 or n == 1:
+#         return 1
+#     elif n < 0:
+#         raise ValueError("Factorial is not defined for negative numbers")
+#     else:
+#         result = 1
+#         for i in range(2, n + 1):
+#             result *= i
+#         return result
+
+
+def factorial(n):
+    if n < 0:
+        raise ValueError("Factorial is not defined for negative numbers")
+    result = 1
+    for i in range(2, n + 1):
+        result *= i
+    return result
